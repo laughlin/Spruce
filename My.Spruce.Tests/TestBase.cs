544e4bc7832634e0bc8b1f00d2eb7dc7ec403218
uofifox <sfox@laughlin.com>
2017-08-14 11:06:23 -0500

    [my.spruce] test setup

---
+++ My.Spruce.Tests/TestBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using NUnit.Framework;
+using StructureMap;
+
+namespace My.Spruce.Tests
+{
+	public abstract class TestBase
+	{
+		protected IDbConnection Db { get; set; }
+
+		[TestFixtureSetUp]
+		public virtual void SetupFixture()
+		{
+			Db = ObjectFactory.GetInstance<IDbConnection>();
+		}
+		[TestFixtureTearDown]
+		public virtual void TearDownFixture()
+		{
+		}
+
+		[SetUp]
+		public virtual void Setup()
+		{
+		}
+
+		[TearDown]
+		public virtual void TearDown()
+		{
+		}
+	}
+}
---
+++ My.Spruce.Tests/TestFixtureSetup.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using My.Spruce.Schema;
+using My.Spruce.Tests.Infrastructure;
+using NUnit.Framework;
+using StackExchange.Profiling;
+using StructureMap;
+
+namespace My.Spruce.Tests
+{
+	[SetUpFixture]
+	public class TestFixtureSetup
+	{
+		[SetUp]
+		public void Setup()
+		{
+			MiniProfiler.Settings.ProfilerProvider = new SingletonProfilerProvider();
+			MiniProfiler.Start();
+			ObjectFactory.Initialize(x => x.AddRegistry(new IocRegistry()));
+
+			// Start with a fresh, empty db
+			var db = ObjectFactory.GetInstance<IDbConnection>();
+			db.DropAllScriptedObjects();
+			db.DropAllTables();
+		}
+	}
+}
